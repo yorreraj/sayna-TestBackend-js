bec093d9a1032ddf8de4b3dc38cec5c2d737665f
yorre <rajaonariveloyorre@gmail.com>
2021-12-19 18:12:55 +0300

    update

--- src/server.js
+++ src/server.js
@@ -55,10 +55,10 @@ app.use(async (err, req, res) => {
 //routes definitions
 app.use([userRouter, cardRouter, songRouter, subscriptionRouter])
 
-//mongoose.connect(process.env.MONGO_URL).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`)
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`)
     })
-//})
+})
 
 
